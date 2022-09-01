844ffd3da22ba4317e2f43d9f60edbcc9245fae8
seita1996 <taha.seita@gmail.com>
2022-09-01 10:48:08 +0900

    Acquire video from the device's camera and display it on the screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,4 +5,20 @@ function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
+(async function(){
+  // カメラ映像取得
+  const localMediaStream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      width: 1280,
+      height: 720,
+      frameRate: 8
+    },
+    audio: false
+  }) as MediaStream
+  const localVideo = document.getElementById('localVideo') as HTMLVideoElement
+  if(localVideo !== null) {
+    localVideo.srcObject = localMediaStream
+  }
+})()
+
 export default MyApp
