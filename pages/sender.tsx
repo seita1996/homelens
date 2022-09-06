c7672b1d731124d8947ccf3e7483324d5e924b22
seita1996 <taha.seita@gmail.com>
2022-09-06 10:50:46 +0900

    Add connect function

--- pages/sender.tsx
+++ pages/sender.tsx
@@ -75,6 +75,7 @@ const Sender: NextPage = () => {
   // }
 
   let localStream: MediaStream
+  let peerConnection: RTCPeerConnection
 
   async function startVideo() {
     console.log('startVideo')
@@ -82,14 +83,24 @@ const Sender: NextPage = () => {
     localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
     playVideo(localVideo, localStream)
   }
+
   function stopVideo() {
     console.log('stopVideo')
     const localVideo = document.getElementById('localVideo') as HTMLVideoElement
     pauseVideo(localVideo)
     stopLocalStream(localStream)
   }
+
+  // Start PeerConnection
   function connect() {
     console.log('connect')
+    if (! peerConnection) {
+      console.log('make Offer')
+      makeOffer()
+    }
+    else {
+      console.warn('peer already exist.')
+    }
   }
   function hangup() {
     console.log('hangup')
@@ -107,6 +118,7 @@ const Sender: NextPage = () => {
     element.play()
     element.volume = 0
   }
+
   function pauseVideo(element: HTMLMediaElement) {
     element.pause()
     if ('srcObject' in element) {
@@ -119,6 +131,7 @@ const Sender: NextPage = () => {
       element.src = ''
     }
   }
+
   function stopLocalStream(stream: MediaStream) {
     let tracks = stream.getTracks()
     if (! tracks) {
@@ -128,6 +141,113 @@ const Sender: NextPage = () => {
     for (let track of tracks) {
       track.stop()
     }
+  }
+
+  function prepareNewConnection() {
+    let pc_config = {"iceServers":[]}
+    let peer = new RTCPeerConnection(pc_config)
+
+    // このロジックはReceiverで必要
+    // --- on get remote stream ---
+    // if ('ontrack' in peer) {
+    //   peer.ontrack = function(event) {
+    //     console.log('-- peer.ontrack()')
+    //     let stream = event.streams[0]
+    //     playVideo(remoteVideo, stream)
+    //     if (event.streams.length > 1) {
+    //       console.warn('got multi-stream, but play only 1 stream')
+    //     }
+    //   }
+    // }
+    // else {
+    //   peer.onaddstream = function(event) {
+    //     console.log('-- peer.onaddstream()')
+    //     let stream = event.stream
+    //     playVideo(remoteVideo, stream)
+    //   };
+    // }
+
+    // --- on get local ICE candidate
+    peer.onicecandidate = function (evt) {
+      if (evt.candidate) {
+        console.log(evt.candidate)
+
+        // Trickle ICE の場合は、ICE candidateを相手に送る
+        // Vanilla ICE の場合には、何もしない
+      } else {
+        console.log('empty ice event')
+
+        // Trickle ICE の場合は、何もしない
+        // Vanilla ICE の場合には、ICE candidateを含んだSDPを相手に送る
+        sendSdp(peer.localDescription as RTCSessionDescription)
+      }
+    }
+
+    // -- add local stream --
+    if (localStream) {
+      console.log('Adding local stream...')
+      if ('addTrack' in peer) {
+        console.log('use addTrack()')
+        let tracks = localStream.getTracks()
+        for (let track of tracks) {
+          let sender = peer.addTrack(track, localStream)
+        }
+      }
+      else {
+        console.log('use addStream()')
+        peer.addStream(localStream)
+      }
+    }
+    else {
+      console.warn('no local stream, but continue.')
+    }
+
+    return peer
+  }
+
+  function makeOffer() {
+    peerConnection = prepareNewConnection()
+
+    let options = {}
+    if (localStream) {
+      console.log('-- try sendonly ---')
+      options = { offerToReceiveAudio: false, offerToReceiveVideo: false }
+    }
+    // このロジックはReceiveで必要
+    // else {
+    //   // -- no localStream, so receive --
+    //   console.log('-- try recvonly ---')
+
+    //   options = { offerToReceiveAudio: true, offerToReceiveVideo: true }
+
+    //   if ('addTransceiver' in peerConnection) {
+    //     console.log('-- use addTransceiver() for recvonly --')
+    //     peerConnection.addTransceiver('video').setDirection('recvonly')
+    //     peerConnection.addTransceiver('audio').setDirection('recvonly')
+    //   }
+    // }
+
+    peerConnection.createOffer(options)
+    .then(function (sessionDescription) {
+      console.log('createOffer() succsess in promise')
+      return peerConnection.setLocalDescription(sessionDescription)
+    }).then(function() {
+      console.log('setLocalDescription() succsess in promise')
+
+      // -- Trickle ICE の場合は、初期SDPを相手に送る --
+      // -- Vanilla ICE の場合には、まだSDPは送らない --
+      //sendSdp(peerConnection.localDescription);
+    }).catch(function(err) {
+      console.error(err)
+    });
+  }
+
+  function sendSdp(sessionDescription: RTCSessionDescription) {
+    console.log('---sending sdp ---')
+    const textForSendSdp: HTMLTextAreaElement = document.getElementById('text_for_send_sdp') as HTMLTextAreaElement
+    textForSendSdp.value = sessionDescription.sdp
+    textForSendSdp.focus()
+    textForSendSdp.select()
   }
 
   const title: string = "title"
@@ -159,6 +279,15 @@ const Sender: NextPage = () => {
           title={"canditates"}
           description={JSON.stringify(canditates)}
         />
+        <p>SDP to send:&nbsp;
+          <button type="button">copy local SDP</button><br />
+          <textarea id="text_for_send_sdp" rows="5" cols="60" readonly="readonly">SDP to send</textarea>
+        </p>
+        {/* Receive実装時にアンコメント */}
+        {/* <p>SDP to receive:&nbsp;
+          <button type="button" onClick={onSdpText}>Receive remote SDP</button><br />
+          <textarea id="text_for_receive_sdp" rows="5" cols="60"></textarea>
+        </p> */}
       </div>
     </div>
   )
