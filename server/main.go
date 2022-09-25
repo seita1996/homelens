d33691f8b0c18ee318984e48da372882a6f3c317
seita1996 <taha.seita@gmail.com>
2022-09-25 10:34:27 +0900

    Add WebSocket Room and Client logic

--- server/main.go
+++ server/main.go
@@ -1,42 +1,115 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"time"
 	"net/http"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"golang.org/x/net/websocket"
 )
 
+type Client struct {
+	socket	*websocket.Conn	// WebSocket Connection
+	send	chan string		// Channel to send a message to the browser
+	room	*Room			// The chat room to which the client belongs
+	name	string
+}
+
+type Room struct {
+	forward	chan string			// With a message to send to all other clients
+	clients	map[string]Client	// All clients in the room
+}
+
+var rooms = make(map[string]Room)
+
+func newRoom(name string) *Room {
+	fmt.Println("[newRoom()] New Room name: " + name)
+	room := &Room {
+		forward:	make(chan string),
+		clients:	make(map[string]Client),
+	}
+	printStruct("[newRoom()] Room num", len(rooms))
+	if _, ok := rooms[name]; ok {
+		fmt.Println("[newRoom()] The Room Exists")
+	} else {
+		fmt.Println("[newRoom()] Create New Room")
+		rooms[name] = *room
+		printStruct("[newRoom()] Room num", len(rooms))
+	}
+	return room
+}
+
+func printStruct(title string, stru any) {
+	jsons, _ := json.Marshal(stru)
+	fmt.Println(title + ": " + string(jsons))
+}
+
+func (r *Room) join(c *Client) {
+	r.clients[c.name] = *c
+
+	// debug
+	printStruct("[join()] c name", &c.name)
+}
+
+func (r *Room) leave(c *Client) {
+	delete(r.clients, c.name)
+
+	// debug
+	printStruct("[leave()] c name", &c.name)
+}
+
 func handleWebSocket(c echo.Context) error {
 	websocket.Handler(func(ws *websocket.Conn) {
 		defer ws.Close()
 
-		// 初回のメッセージを送信
+		fmt.Println("[handleWebSocket()] 接続元IP: " + c.RealIP())
+		r := newRoom(c.RealIP())
+		client := &Client {
+			socket: ws,
+			send:   make(chan string),
+			room:   r,
+			name:	time.Now().Format("2006-01-02 15:04:05"),
+		}
+		printStruct("[handleWebSocket()] Clients in Room num", len(rooms[c.RealIP()].clients))
+		fmt.Println("[handleWebSocket()] join!!!")
+		r.join(client)
+		printStruct("[handleWebSocket()] Clients in Room num", len(rooms[c.RealIP()].clients))
+
+		defer func() {
+			printStruct("[handleWebSocket()] Clients in Room num", len(rooms[c.RealIP()].clients))
+			fmt.Println("[handleWebSocket()] leave!!!")
+			r.leave(client)
+			printStruct("[handleWebSocket()] Clients in Room num", len(rooms[c.RealIP()].clients))
+		}()
+
+		// Send initial message
 		err := websocket.Message.Send(ws, "ping")
 		if err != nil {
 			c.Logger().Error(err)
 		}
 
 		for {
-			fmt.Println("接続元IP: " + c.RealIP())
+			fmt.Println("[handleWebSocket()] 接続元IP: " + c.RealIP())
 
-			// Client からのメッセージを読み込む
+			// Read messages from Client
 			msg := ""
 			err = websocket.Message.Receive(ws, &msg)
 			if err != nil {
 				c.Logger().Error(err)
 				break
 			}
-			fmt.Println("Receive: " + msg)
+			client.room.forward <- msg
+			fmt.Println("[handleWebSocket()] Receive: " + msg)
 
-			// Client からのメッセージを元に返すメッセージを作成し送信する
+			// Create a message to return based on the message from Client and send it
 			err := websocket.Message.Send(ws, fmt.Sprintf("Server: \"%s\" received!", msg))
 			if err != nil {
 				c.Logger().Error(err)
 				break
 			}
-			fmt.Println("Send: " + msg)
+			fmt.Println("[handleWebSocket()] Send: " + msg)
 		}
 	}).ServeHTTP(c.Response(), c.Request())
 	return nil
