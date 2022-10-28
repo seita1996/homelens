9c24368e6646369dc21b4a2ec669ae51581d87f3
seita1996 <taha.seita@gmail.com>
2022-10-28 15:45:02 +0900

    Add Switch to sidebar

--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -1,13 +1,31 @@
 import styles from '../styles/Sidebar.module.css'
 import { ReactSVG } from 'react-svg'
+import Switch from 'react-switch'
+import { useState } from 'react'
 
 const Sidebar = (props: { visible: boolean, closeSideBar: Function}) => {
+  const [enableBackCamera, setEnableBackCamera] = useState(false)
+
+  function handleChange() {
+    setEnableBackCamera(!enableBackCamera)
+  }
+
   if(props.visible) {
     return (
-      <div className={`${styles.sideBox} ${styles.flexSpaceBetween}`}>
-        <div className={styles.title}>設定</div>
-        <div className={styles.iconBox}>
-          <ReactSVG src="/xmark.svg" className={styles.deviceicon} style={{ width: "32px", height: "32px" }} onClick={() => {props.closeSideBar()}} />
+      <div className={styles.sideBox}>
+        <div className={styles.flexSpaceBetween}>
+          <div className={styles.title}>設定</div>
+          <div className={styles.iconBox}>
+            <ReactSVG src="/xmark.svg" className={styles.deviceicon} style={{ width: "32px", height: "32px" }} onClick={() => {props.closeSideBar()}} />
+          </div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.flexSpaceBetween}>
+            <div>
+              背面カメラを使う
+            </div>
+            <Switch onChange={handleChange} checked={enableBackCamera} onColor={'#F2994A'} />
+          </div>
         </div>
       </div>
     )
