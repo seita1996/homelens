b5efab3ceb92db3ac049f5c06b68661f86e571df
seita1996 <taha.seita@gmail.com>
2022-11-16 21:10:48 +0900

    Delete terms pages test

--- client/__tests__/pages/terms.spec.tsx
+++
@@ -1,9 +0,0 @@
-import Terms from "@/pages/terms"
-import { render } from "@testing-library/react"
-
-describe("Terms", () => {
-  test("should exist", () => {
-    const { getByText } = render(<Terms />)
-    expect(getByText("Terms")).toBeTruthy()
-  })
-})
